30033748f629493608260917ada0b6d4890179da
basant haider <105054584+basanthaider@users.noreply.github.com>
2024-04-06 03:53:49 +0200

    Update DeleteBookController.java
    add the delete book controller

--- src/main/java/com/project/librarySystem/book/controller/DeleteBookController.java
+++ src/main/java/com/project/librarySystem/book/controller/DeleteBookController.java
@@ -6,4 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class DeleteBookController {
+     @Autowired
+    private DeleteBookServices deleteBookServices;
+
+    @RequestMapping("/deleteBook/{id}")
+    public ResponseEntity<String> deleteBookById(@PathVariable int id) {
+       deleteBookServices.deleteById(id);
+        return ResponseEntity.ok("Book with ID " + id + " deleted successfully");
+    }
+
+    // @PostMapping("/deleteBook")
+// public ResponseEntity<Void> deleteBook(@RequestBody Book book) {
+//     bookService.deleteBook(book);
+//     return ResponseEntity.noContent().build();// Return a 204 No Content response
+// }
+  
 }
