925cebcf951f97d38965fcb3839a2e89962d9194
basant haider <105054584+basanthaider@users.noreply.github.com>
2024-04-06 03:51:51 +0200

    Update DeleteBookServices.java
    add the delete book service

--- src/main/java/com/project/librarySystem/book/services/DeleteBookServices.java
+++ src/main/java/com/project/librarySystem/book/services/DeleteBookServices.java
@@ -4,4 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DeleteBookServices {
+   @Autowired
+    private BookRepo bookRepo;
+    public void delete(Book book) {
+        bookRepo.delete(book);
+    }
+
+    public void deleteById(int id) {
+        bookRepo.deleteById(id);
+    }
 }
