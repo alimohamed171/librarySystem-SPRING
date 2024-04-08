b03a8db8694e60d438b16fd3bbf94c6876d50c36
AliMohamed <alimohammed1712003@gmail.com>
2024-04-09 00:22:41 +0200

    updates

--- src/main/java/com/project/librarySystem/models/Book.java
+++ src/main/java/com/project/librarySystem/models/Book.java
@@ -1,10 +1,12 @@
 package com.project.librarySystem.models;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Set;
 
 @Entity
 @Table(name = "books")
+@Data
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,73 +18,73 @@ public class Book {
     private int availableCopies;
     private int totalCopies;
 
-    public Book() {
-    }
-
-    public Book(int bookID, String title, String author, String ISBN, int rackNumber, int availableCopies, int totalCopies) {
-        this.bookID = bookID;
-        this.title = title;
-        this.author = author;
-        this.ISBN = ISBN;
-        this.rackNumber = rackNumber;
-        this.availableCopies = availableCopies;
-        this.totalCopies = totalCopies;
-    }
-
-    public int getBookID() {
-        return bookID;
-    }
-
-    public void setBookID(int bookID) {
-        this.bookID = bookID;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getISBN() {
-        return ISBN;
-    }
-
-    public void setISBN(String ISBN) {
-        this.ISBN = ISBN;
-    }
-
-    public int getRackNumber() {
-        return rackNumber;
-    }
-
-    public void setRackNumber(int rackNumber) {
-        this.rackNumber = rackNumber;
-    }
-
-    public int getAvailableCopies() {
-        return availableCopies;
-    }
-
-    public void setAvailableCopies(int availableCopies) {
-        this.availableCopies = availableCopies;
-    }
-
-    public int getTotalCopies() {
-        return totalCopies;
-    }
-
-    public void setTotalCopies(int totalCopies) {
-        this.totalCopies = totalCopies;
-    }
+//    public Book() {
+//    }
+//
+//    public Book(int bookID, String title, String author, String ISBN, int rackNumber, int availableCopies, int totalCopies) {
+//        this.bookID = bookID;
+//        this.title = title;
+//        this.author = author;
+//        this.ISBN = ISBN;
+//        this.rackNumber = rackNumber;
+//        this.availableCopies = availableCopies;
+//        this.totalCopies = totalCopies;
+//    }
+//
+//    public int getBookID() {
+//        return bookID;
+//    }
+//
+//    public void setBookID(int bookID) {
+//        this.bookID = bookID;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getAuthor() {
+//        return author;
+//    }
+//
+//    public void setAuthor(String author) {
+//        this.author = author;
+//    }
+//
+//    public String getISBN() {
+//        return ISBN;
+//    }
+//
+//    public void setISBN(String ISBN) {
+//        this.ISBN = ISBN;
+//    }
+//
+//    public int getRackNumber() {
+//        return rackNumber;
+//    }
+//
+//    public void setRackNumber(int rackNumber) {
+//        this.rackNumber = rackNumber;
+//    }
+//
+//    public int getAvailableCopies() {
+//        return availableCopies;
+//    }
+//
+//    public void setAvailableCopies(int availableCopies) {
+//        this.availableCopies = availableCopies;
+//    }
+//
+//    public int getTotalCopies() {
+//        return totalCopies;
+//    }
+//
+//    public void setTotalCopies(int totalCopies) {
+//        this.totalCopies = totalCopies;
+//    }
 
 }
--- src/main/java/com/project/librarySystem/models/User.java
+++ src/main/java/com/project/librarySystem/models/User.java
@@ -1,11 +1,13 @@
 package com.project.librarySystem.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Set;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,65 +19,65 @@ public class User {
     private String userType;
 
 
-    public User() {
-    }
-
-    public User(int userID, String username, String password, String email, String phoneNumber, String userType) {
-        this.userID = userID;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.userType = userType;
-
-    }
-
-    public int getUserID() {
-        return userID;
-    }
-
-    public void setUserID(int userID) {
-        this.userID = userID;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getUserType() {
-        return userType;
-    }
-
-    public void setUserType(String userType) {
-        this.userType = userType;
-    }
+//    public User() {
+//    }
+//
+//    public User(int userID, String username, String password, String email, String phoneNumber, String userType) {
+//        this.userID = userID;
+//        this.username = username;
+//        this.password = password;
+//        this.email = email;
+//        this.phoneNumber = phoneNumber;
+//        this.userType = userType;
+//
+//    }
+//
+//    public int getUserID() {
+//        return userID;
+//    }
+//
+//    public void setUserID(int userID) {
+//        this.userID = userID;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//
+//    public void setPhoneNumber(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
+//
+//    public String getUserType() {
+//        return userType;
+//    }
+//
+//    public void setUserType(String userType) {
+//        this.userType = userType;
+//    }
 
 }
