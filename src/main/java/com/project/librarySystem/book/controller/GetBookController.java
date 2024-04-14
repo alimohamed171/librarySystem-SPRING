49fc4c56696079cd02116e60f4edf68cd9ae443b
BaSant <basssnthaider@gamil.com>
2024-04-14 19:27:18 +0200

    book operations

--- src/main/java/com/project/librarySystem/book/controller/GetBookController.java
+++ src/main/java/com/project/librarySystem/book/controller/GetBookController.java
@@ -3,6 +3,7 @@ package com.project.librarySystem.book.controller;
 import com.project.librarySystem.book.services.GetBookServices;
 import com.project.librarySystem.models.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import java.util.List;
 public class GetBookController {
     @Autowired
     private GetBookServices getBookServices;
+    @GetMapping("/getAllBooks")
     public List<Book> getAllBooks(){
         return  getBookServices.getAllBooks();
     }
