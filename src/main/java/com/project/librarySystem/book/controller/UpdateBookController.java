0a4118e519b8795a81659862030c0c57e438b935
BaSant <basssnthaider@gamil.com>
2024-04-16 23:12:05 +0200

    book operations

--- src/main/java/com/project/librarySystem/book/controller/UpdateBookController.java
+++ src/main/java/com/project/librarySystem/book/controller/UpdateBookController.java
@@ -6,15 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping
 public class UpdateBookController {
     @Autowired
     UpdateBookServices updateBookServices;
     @PutMapping("/updateBook/{id}")
-    public ResponseEntity<String>updateBook(@PathVariable int id, @RequestBody Book bookDetails){
-        String message =updateBookServices.updateBook(id,bookDetails);
-        return ResponseEntity.ok(message);
+    public ResponseEntity<?>updateBook(@PathVariable int id, @RequestBody Book bookDetails){
+        Optional<?> updatedBook =updateBookServices.updateBook(id,bookDetails);
+        return ResponseEntity.ok(updatedBook);
 
     }
 }
--- src/main/java/com/project/librarySystem/book/services/UpdateBookServices.java
+++ src/main/java/com/project/librarySystem/book/services/UpdateBookServices.java
@@ -5,20 +5,32 @@ import com.project.librarySystem.repository.BookRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UpdateBookServices {
     @Autowired
     private BookRepo bookRepo;
-    public String updateBook(int id, Book bookDetails){
-
-        if(bookIsExist(id)){
-            bookRepo.save(bookDetails);
-            return "book is updated";
+    public Optional<?> updateBook(int id, Book bookDetails) {
+        Optional<Book> existingBook = bookRepo.findById(id);
+        if (existingBook.isPresent()) {
+            existingBook.get().setTitle(bookDetails.getTitle());
+            existingBook.get().setAuthor(bookDetails.getAuthor());
+            existingBook.get().setISBN(bookDetails.getISBN());
+            existingBook.get().setRackNumber(bookDetails.getRackNumber());
+            existingBook.get().setAvailableCopies(bookDetails.getAvailableCopies());
+            existingBook.get().setTotalCopies(bookDetails.getTotalCopies());
+            return Optional.of(bookRepo.save(existingBook.get()));
         }
-        return "book is not exist";
+        return Optional.of(("Book with id " + id + " not found"));
     }
+
     private boolean bookIsExist(int id) {
         return bookRepo.findById(id).isPresent();
     }
-
 }
+
+
+
+
+
