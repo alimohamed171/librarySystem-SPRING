a37b477081bca161112342f5008ae6a0d378cbd5
BaSant <basssnthaider@gamil.com>
2024-05-04 18:28:03 +0300

    adding react

--- src/main/java/com/project/librarySystem/repository/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.project.librarySystem.repository;
-
-import com.project.librarySystem.models.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepo extends JpaRepository<Book,Integer> {
-    Book findByTitle(String title);
-    Book findByISBN(String ISBN);
-}
